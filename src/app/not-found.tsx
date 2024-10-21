9dfcd7eb5dc2e8b0f69ff383cc5a02e20f3b7931
Muhammad Noyan  Aziz <noyan.aziz@A006-01474.local>
2024-10-21 06:58:46 +0500

    fix: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,6 +2,7 @@ export default function NotFoundError() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-center text-2xl text-white">404: Page not found</h1>
+      <h2 className="text-center text-white">Make sure the URL is correct</h2>
     </main>
   );
 }
